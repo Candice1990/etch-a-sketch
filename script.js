660be34c23de8f7bae981e4d7e7c15bc46afe8ca
Candice1990 <czech.candice@gmail.com>
2017-03-31 23:46:45 -0400

    update

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function createGrid(x) {
         for (var columns = 0; columns < x; columns++) {
             $("#container").append("<div class='grid'></div>");
         };
+
     };
     $(".grid").width(960/x);
     $(".grid").height(960/x);
