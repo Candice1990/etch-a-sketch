6d44a41ff89fd88aac4228c094a13bac3deab9d8
Candice1990 <czech.candice@gmail.com>
2017-04-01 13:06:54 -0400

    Basic black etch-a-sketch

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 //function for creating the rows and columns of a grid.
-function createGrid(number){
-  for(var i = 0, i < number, i++){
-    for(var i = 0, i < number, i++){
-      $("#container").append("<div class = "grid"></div>");
+
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            $("#container").append("<div class='grid'></div>");
+        };
+
     };
-  };
-  $(".grid").width(960/number);
-  $(".grid").height(960/number);
+    $(".grid").width(500/x);
+    $(".grid").height(500/x);
+};
+
 
 //functon that clears grid for reset
 function clearGrid(){
@@ -15,24 +19,39 @@ function clearGrid(){
 
 //functions that prompts the user to input the size of grid.
 function refreshGrid(){
-  var ask = ("How many squares do you want on each side?");
-  clearGrid();
-  createGrid(ask);
+    var ask = prompt("How many boxes per side? (limit 50)");
+    if (ask < 51){
+      clearGrid();
+      createGrid(ask);
+    } else {
+      var newAsk = prompt("Choose between 0 and 50.")
+        if (newAsk < 51) {
+          clearGrid();
+          createGrid(newAsk);
+      } else {
+        alert("Goddamn it, what did I say?");
+      };
+
+    };
+
 };
 
 /*creates a 16 by 16 grid where as the user's mouse hovers over a given
 square, the square darkens*/
-$(document).ready(){
-  createGrid(16);
-}
-$(".grid").mouseover(function(){
-  $(this).css("background-color","black");
-});
-$(".newGrid").click(function() {
-       refreshGrid();
+$(document).ready(function() {
+    createGrid(25);
 
-       $(".grid").mouseover(function() {
-       $(this).css("background-color", "black");
-       });
-   });
+    $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+        $(this).css("transition", "0.1s");
+        });
+
+    $(".newGrid").click(function() {
+        refreshGrid();
+
+        $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+        $(this).css("transition", "0.1s");
+        });
+    });
 });
