597e77892cf31242361467fcd98627fd80c3dca5
Candice1990 <czech.candice@gmail.com>
2017-04-04 11:43:24 -0400

    Working on rainbow option

--- script.js
+++ script.js
@@ -23,6 +23,9 @@ function refreshGrid(){
     if (ask < 51){
       clearGrid();
       createGrid(ask);
+    } if (ask === null){
+      clearGrid();
+      createGrid(25);
     } else {
       var newAsk = prompt("Choose between 0 and 50.")
         if (newAsk < 51) {
@@ -36,22 +39,38 @@ function refreshGrid(){
 
 };
 
+/* creates a function for each different colouring options
+function blackDefault(){
+  $(this).css("background-color": "black", "transition": "0.1s")
+}
+
+function rainbowColors(){
+  var newColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+        $(this).css({"background-color": newColor,
+                     "opacity": "1.0"});
+}; */
+
+
 /*creates a 16 by 16 grid where as the user's mouse hovers over a given
 square, the square darkens*/
+var style = "default";
 $(document).ready(function() {
     createGrid(25);
 
     $(".grid").mouseover(function() {
-        $(this).css("background-color", "black");
-        $(this).css("transition", "0.1s");
+        $(this).css("background-color", "black", "transition", "0.1s");
         });
 
     $(".newGrid").click(function() {
         refreshGrid();
 
         $(".grid").mouseover(function() {
-        $(this).css("background-color", "black");
-        $(this).css("transition", "0.1s");
+        $(this).css("background-color", "black", "transition", "0.1s");
         });
     });
+  /*  $("#rainbow").click(function(){
+      style = "rainbow";
+      toggleStyle("rainbowColors");
+
+    })*/
 });
